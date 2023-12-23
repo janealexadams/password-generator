7058cafbc4fc3d15629b9245b788909c29d06bef
janealexadams <janealexadams@gmail.com>
2023-12-23 13:37:40 -0800

    updated javascript

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberChars = "0123456789";
 var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-// Make variable for the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -17,79 +16,70 @@ function writePassword() {
 // When I click on the button the writePassword will run
 generateBtn.addEventListener('click', writePassword);
 
-
 function generatePassword() {
 
-var userChoice = "" // carring the information - this is the empty plate
-var generatedPassword = ""
+  var userChoice = "" 
+  var generatedPassword = ""
 
-// Number of characters prompt
-  // The prompt when the generate button is clicked
-  var passUserInput = prompt("Choose a length of at least 8 characters and no more than 128 characters.");
-  console.log(passUserInput);
+  // Number of characters prompt
+    // The prompt when the generate button is clicked
+    var passUserInput = prompt("Choose a length of at least 8 characters and no more than 128 characters.");
+    console.log(passUserInput);
 
-  // If user pressed Cancel, immediately end function
-if (!passUserInput) {
-  return;
-}
+    // If user pressed Cancel, immediately end function
+  if (!passUserInput) {
+    return;
+  }
 
-if (passUserInput < 8) {
-  alert("Password must have more than 7 characters!");
-  return;
-}
+  if (passUserInput < 8) {
+    alert("Password must have more than 7 characters!");
+    return;
+  }
 
-if (passUserInput > 128) {
-  alert("Password must have less than 128 characters!");
-  return;
-}
+  if (passUserInput > 128) {
+    alert("Password must have less than 128 characters!");
+    return;
+  }
 
-// });
+  // Lowercase characters prompt
+  var lowercaseUserInput = confirm("Would you like lowercase characters (y/n)?");
 
+  if (lowercaseUserInput === true) {
+  userChoice = userChoice + lowercaseChars
+  }
 
-// Lowercase characters prompt
-var lowercaseUserInput = confirm("Would you like lowercase characters (y/n)?");
+  // Uppercase characters prompt
+  var uppercaseUserInput = confirm("Would you like uppercase characters (y/n)?");
 
-if (lowercaseUserInput === true) {
-userChoice = userChoice + lowercaseChars
-}
+  if (uppercaseUserInput === true) {
+    userChoice = userChoice + uppercaseChars
+  }
 
+  // Numeric characters prompt
+  var nuUserInput = confirm("Would you like numeric characters (y/n)?");
 
-// Uppercase characters prompt
-var uppercaseUserInput = confirm("Would you like uppercase characters (y/n)?");
+  if (nuUserInput === true) {
+    userChoice = userChoice + numberChars
+  }
 
-if (uppercaseUserInput === true) {
-  userChoice = userChoice + uppercaseChars
-}
+  // Special characters prompt
+  var spUserInput = confirm("Would you like special characters (y/n)?");
 
+  if (spUserInput === true) {
+    userChoice = userChoice + specialChars
+  }
 
-// Numeric characters prompt
-var nuUserInput = confirm("Would you like numeric characters (y/n)?");
+  // Making the password
+  userChoice.charAt(Math.floor(Math.floor(Math.random() * userChoice.length)))
+  console.log(userChoice)
 
-if (nuUserInput === true) {
-  userChoice = userChoice + numberChars
-}
-
-
-// Special characters prompt
-var spUserInput = confirm("Would you like special characters (y/n)?");
-
-if (spUserInput === true) {
-  userChoice = userChoice + specialChars
-}
-
-
-
-// Making the password
-userChoice.charAt(Math.floor(Math.floor(Math.random() * userChoice.length)))
-console.log(userChoice)
-
-for (var i = 0; i < passUserInput; i++) {
-  generatedPassword = generatedPassword + userChoice.charAt(Math.floor(Math.floor(Math.random() * userChoice.length)))
+  for (var i = 0; i < passUserInput; i++) {
+    generatedPassword = generatedPassword + userChoice.charAt(Math.floor(Math.floor(Math.random() * userChoice.length)))
+    console.log(generatedPassword)
+  }
   console.log(generatedPassword)
-}
-console.log(generatedPassword)
 
-return generatedPassword
+  return generatedPassword
 
 }
 
